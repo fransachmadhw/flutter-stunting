811851539632004c9e5d0f3c765923367986476b
frans <franswinata6@gmail.com>
2022-11-03 17:29:34 +0700

    Colors update

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,49 @@
 library flutter_stunting.globals;
 
+import 'dart:ui';
+
 // Global Variables here
+
 String title = "Flutter Stunting";
+
+// Colour
+const Color primary50 = Color(0xFFDAF0FB);
+const Color primary100 = Color(0xFFB5E1F6);
+const Color primary200 = Color(0xFF91D2F2);
+const Color primary300 = Color(0xFF6CC2EE);
+const Color primary400 = Color(0xFF47B3E9);
+const Color primary500 = Color(0xFF22A4E5);
+
+const Color white = Color(0xFFFFFFFF);
+const Color neutral50 = Color(0xFFE8E8E8);
+const Color neutral100 = Color(0xFFD1D1D1);
+const Color neutral200 = Color(0xFFB9B9B9);
+const Color neutral300 = Color(0xFFA2A2A2);
+const Color neutral400 = Color(0xFF8B8B8B);
+const Color neutral500 = Color(0xFF747474);
+const Color neutral600 = Color(0xFF5D5D5D);
+const Color neutral700 = Color(0xFF464646);
+const Color neutral800 = Color(0xFF2E2E2E);
+const Color neutral900 = Color(0xFF171717);
+const Color black = Color(0xFF000000);
+
+const Color red50 = Color(0xFFF7D5D5);
+const Color red100 = Color(0xFFEFAAAA);
+const Color red200 = Color(0xFFE88080);
+const Color red300 = Color(0xFFE05555);
+const Color red400 = Color(0xFFD82A2A);
+const Color red500 = Color(0xFFD00000);
+
+const Color yellow50 = Color(0xFFFFF8D6);
+const Color yellow100 = Color(0xFFFFF1AD);
+const Color yellow200 = Color(0xFFFFEB85);
+const Color yellow300 = Color(0xFFFFE45C);
+const Color yellow400 = Color(0xFFFFDD33);
+const Color yellow500 = Color(0xFFFFD60A);
+
+const Color green50 = Color(0xFFD5EAE4);
+const Color green100 = Color(0xFFAAD4CA);
+const Color green200 = Color(0xFF80BFAF);
+const Color green300 = Color(0xFF55AA94);
+const Color green400 = Color(0xFF2A947A);
+const Color green500 = Color(0xFF007F5F);
