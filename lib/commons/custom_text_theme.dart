33eb13702c0d9acebfa6f38b95c94885acc9dd23
IvanReyhan22 <ivanreyhan2002@gmail.com>
2022-11-08 13:25:29 +0700

    (+) Global Text (+) struktur folder

---
+++ lib/commons/custom_text_theme.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stunting/commons/globals.dart';
+
+///
+/// Custom text theme
+/// How to use:
+/// Text("Hellow World",style: Theme.of(context).textTheme.headlineLarge)
+///
+///
+
+const customTextTheme = TextTheme(
+  headlineLarge: TextStyle(
+    fontSize: 42,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.4,
+    color: black,
+  ),
+  headlineMedium: TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.4,
+    color: black,
+  ),
+  headlineSmall: TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.4,
+    color: black,
+  ),
+  titleLarge: TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    color: black,
+  ),
+  bodyLarge: TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+    color: black,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: black,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: black,
+  ),
+);
